cd4412ba5a8983ae5d171d64b869f0e75b9234bf
nisaandriyani <46709355+nisaandriyani@users.noreply.github.com>
2019-01-16 09:04:53 +0700

    Add files via upload

--- init.php
+++ init.php
@@ -2,10 +2,22 @@
 // require_once 'Produk/InfoProduk.php';
 // require_once 'Produk/InfoProduk.php';
 // require_once 'Produk/Komik.php';
-// require_one 'Produk/Game.php';
+// require_once 'Produk/Game.php';
 // require_once 'Produk/CetakInfoProduk.php';
+// require_once 'Produk/User.php';
 
-spl_autoload_register(function($class) {
-	require_once __DIR__ . '/Produk/' . $class . '.php';
-})
- ?>
+//require_one 'Service/User.php';
+
+
+spl_autoload_register(function( $class ) {
+	$class =explode('\\', $class);
+	$class = end($class);
+	require_once __DIR__ .'/Produk/' . $class . '.php';
+});
+
+spl_autoload_register(function( $class ) {
+	$class = explode('\\', $class);
+	$class = end($class);
+	require_once __DIR__ . '/Service/' . $class . '.php';
+});
+
