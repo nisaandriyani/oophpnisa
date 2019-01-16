80f1cf19b0ae62237f13ed16d74d088d5e93ab5a
nisaandriyani <46709355+nisaandriyani@users.noreply.github.com>
2019-01-16 09:04:00 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <?php 
 require_once 'App/init.php';
-$produk1 = new Komik("ILYFROM", "Michelle Zudith", "Paramitha S", 45000,274);
+// $produk1 = new Komik("ILYFROM", "Michelle Zudith", "Paramitha S", 45000,274);
 
-$produk2 = new Game("3600detik", "Steevan William","Nisa A", 50000,55);
+// $produk2 = new Game("3600d3tik", "Steevan William", "Nisa A", 50000,55);
 
-$cetakProduk = new CetakInfoProduk();
-$cetakProduk->tambahProduk( $produk1 );
-$cetakProduk->tambahProduk( $produk2 );
-echo $cetakProduk->cetak();
+// $cetakProduk = new CetakInfoProduk();
+// cetakProduk->tambahProduk( $produk1 );
+// $cetakProduk->tambahProduk( $produk2 );
+// echo $cetakProduk->cetak();
 
-echo "<hr>";
+// echo "<hr>";
 
-new Coba();
+use App\Service\User as ServiceUser;
+use App\Produk\User as ProdukUser;
+
+new ServiceUser();
+echo "<br>";
+new ProdukUser();
 
  ?>
