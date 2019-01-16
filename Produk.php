fd617b40e6b95c3184efda6a7547772200405659
nisaandriyani <46709355+nisaandriyani@users.noreply.github.com>
2019-01-16 09:03:05 +0700

    Add files via upload

--- Produk.php
+++ Produk.php
@@ -1,41 +1,63 @@
 <?php 
 
-class Produk {
-	public $judul = "judul",
-		   $penulis = "penulis",
-		   $penerbit = "penerbit",
-		   $harga = 0;
+Abstract class Produk {
+protected $judul,
+	      $penulis,
+	      $penerbit,
+	      $harga,
+	      $diskon =0;
 
-		   public function getLabel() {
-		   	return "$this->penulis, $this->penerbit";
-		   }
+
+public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0){
+	$this->judul = $judul;
+	$this->penulis = $penulis;
+	$this->penerbit = $penerbit;
+	$this->harga = $harga;
 }
 
-// $produk1 = new Produk();
-// $produk1->judul = "MATI";
-//var_dump($produk1);
+public function setJudul ($judul) {
+	$this->judul = $judul;
+}
 
-//$produk1 = new Produk();
-//$produk2->judul = "Uncharted";
-//$produk->tambahProperty = "ahahha";
-//var_dump($produk2);
+public function getJudul(){
+	return $this->judul;
+}
 
+public function setPenulis ($penulis) {
+	$this->penulis = $penulis;
+}
 
-$produk3 = new Produk();
-$produk3->judul = "ILYFROM";
-$produk3->penulis = "Michelle Zudith";
-$produk3->penerbit = "Paramitha S";
-$produk3->harga = 45000;
-var_dump($produk3);
+public function getPenulis(){
+	return $this->penulis;
+}
 
-$produk2 = new Produk();
-$produk2->judul = "3600detik";
-$produk2->penulis = "Steevan William";
-$produk2->penerbit = "Nisa A";
-$produk2->harga = 50000;
+public function setPenerbit ($penerbit) {
+	$this->penerbit = $penerbit;
+}
 
-echo "Komik : " . $produk3->getLabel();
-echo "<br>";
-echo "game : " . $produk2->getLabel();
+public function getPenerbit(){
+	return $this->penerbit;
+}
 
- ?>
+public function setDiskon( $diskon){
+	$this->diskon = $diskon;
+}
+
+public function getDiskon(){
+	return $this->diskon;
+}
+
+public function setHarga ($harga){
+	$this->harga = $harga;
+}
+
+public function getHarga(){
+	return $this->harga - ($this->harga * $this->diskon / 100);
+}
+
+public function getLabel(){
+	return "$this->penulis, $this->penerbit";
+}
+
+abstract public function getInfo();
+}
